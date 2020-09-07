d3dae3ddcbea1fa30253843687b019107f276630
Shinya Kageyama <kage@negee.net>
2020-09-07 16:43:05 +0900

    add cache

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,25 @@
+'use strict';
+
+const CACHE_NAME = 'static-cache-v1';
+
+const FILES_TO_CACHE = [
+	'./p.html',
+	'./icon-512.png',
+];
+
 self.addEventListener('install', function(e) {
 	console.log('[ServiceWorker] Install');
+	e.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			console.log('[ServiceWorker] Pre-caching offline page');
+		})
+	);
+	self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
 	console.log('[ServiceWorker] Activate');
+	self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {});
